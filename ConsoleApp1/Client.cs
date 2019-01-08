5f34401ed62f6b70fe394fa4d813c7ecc35fa106
Ricardo Alex da Costa soares <ricardo.soares@iplanrio.rio.rj.gov.br>
2019-01-08 17:31:30 -0200

    Commit Inicial

---
+++ ConsoleApp1/Client.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Client
+    {
+        private TcpClient _tcpSocket;
+        private IPAddress _enderecoIP;
+        private bool Conectado;
+        private string _resposta;
+        NetworkStream stream;
+        StreamReader reader;
+
+        public void InicializaConexao()
+        {
+
+            if (!Conectado)
+            {
+                try
+                {
+                    // Trata o endereço IP informado em um objeto IPAdress
+                    _enderecoIP = IPAddress.Parse("10.3.52.25");
+                    // Inicia uma nova conexão TCP com o servidor chat
+                    _tcpSocket = new TcpClient();
+                    _tcpSocket.Connect(_enderecoIP, 5060);
+
+                    // AJuda a verificar se estamos conectados ou não
+                    Conectado = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro : " + ex.Message, "Erro na conexão com servidor");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Cliente já está conectado!");
+            }
+
+
+        }
+
+        public void FechaConexao()
+        {
+            if (Conectado)
+            {
+                _tcpSocket.Close();
+                Conectado = false;
+                stream.Close();
+                reader.Close();
+            }
+            else
+            {
+                Console.WriteLine("Cliente já está desconectado!");
+            }
+            
+        }
+
+        public void EnviarMensagem(string mensagem)
+        {
+            if (Conectado)
+            {
+                byte[] msg = Encoding.UTF8.GetBytes(mensagem);
+                NetworkStream stream = _tcpSocket.GetStream();
+                try
+                {
+                    stream.Write(msg, 0, msg.Length);
+                    ReceberMensagem();
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                    FechaConexao();
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Cliente Desconectado, para enviar mensagens é necessário que o cliente esteja conectado!");
+            }            
+
+        }
+
+        public void ReceberMensagem()
+        {
+            stream = _tcpSocket.GetStream();
+            reader = new StreamReader(stream, Encoding.UTF8);
+            
+                while (reader.Peek() >= 0)
+                {
+                    Console.Write((char)reader.Read());
+                }            
+            
+        }
+
+        
+
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static bool isclosing = false;
+
+        static void Main(string[] args)
+        {
+            Client cliente = new Client();
+
+            SetConsoleCtrlHandler(new HandlerRoutine(ConsoleCtrlCheck), true);
+
+
+            while (true)
+            {
+
+                switch (Console.ReadLine())
+                {
+                    case "connect":
+                        cliente.InicializaConexao();
+                        break;
+                    case "disconnect":
+                        cliente.FechaConexao();
+                        break;
+                    case "send":
+                        Console.WriteLine("Digite a mensagem");
+                        string msg = Console.ReadLine();
+                        cliente.EnviarMensagem(msg);
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+            
+
+        }
+
+        [DllImport("Kernel32")]
+
+        public static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
+
+        public delegate bool HandlerRoutine(CtrlTypes CtrlType, Client cliente);
+
+        public enum CtrlTypes
+
+        {
+
+            CTRL_C_EVENT = 0,
+
+            CTRL_BREAK_EVENT,
+
+            CTRL_CLOSE_EVENT,
+
+            CTRL_LOGOFF_EVENT = 5,
+
+            CTRL_SHUTDOWN_EVENT
+
+        }
+
+        private static bool ConsoleCtrlCheck(CtrlTypes ctrlType, Client cliente)
+
+        {
+
+            // Put your own handler here
+
+            switch (ctrlType)
+
+            {
+
+                case CtrlTypes.CTRL_C_EVENT:
+
+                    isclosing = true;
+                    cliente.FechaConexao();
+                    Console.WriteLine("CTRL+C received!");
+
+                    break;
+
+
+
+                case CtrlTypes.CTRL_BREAK_EVENT:
+
+                    isclosing = true;
+                    cliente.FechaConexao();
+                    Console.WriteLine("CTRL+BREAK received!");
+
+                    break;
+
+
+
+                case CtrlTypes.CTRL_CLOSE_EVENT:
+
+                    isclosing = true;
+
+                    cliente.FechaConexao();
+
+                    Console.WriteLine("Program being closed!");
+
+                    break;
+
+
+
+                case CtrlTypes.CTRL_LOGOFF_EVENT:
+                    cliente.FechaConexao();
+                    break;
+
+                case CtrlTypes.CTRL_SHUTDOWN_EVENT:
+
+                    isclosing = true;
+                    cliente.FechaConexao();
+                    Console.WriteLine("User is logging off!");
+
+                    break;
+
+
+
+            }
+
+            return true;
+
+        }
+
+    }
+}
